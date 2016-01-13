3be55515aeaaeb1e117ec0c6dc03ca611146c7d5
Ant Cooper <ant@outsrc.co.uk>
2016-01-13 13:55:22 +0000

    Setup mandrill

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'hello@outsrc.co.uk', 'name' => 'Outsrc Intranet'],
 
     /*
     |--------------------------------------------------------------------------
